7ed4ab45736be67bc26423cf00fc6cec3fc64864
olingomaxwell1999 <olingomaxwell1999@gmail.com>
2023-06-03 04:15:05 +0300

    google analytics update

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import '@/styles/globals.css'
 
 import Layout from '@/components/Layout'
+import Script from 'next/script'
 
 function MyApp({Component, pageProps}) {
 
   return (
     <Layout>
+
+      <Script strategy='afterInteractive' src='https://www.googletagmanager.com/gtag/js?id=G-2WTTD0XMFH'/>
+
+        <Script id='google-analytics' strategy='afterInteractive'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2WTTD0XMFH', {
+              page_path: window.location.pathname
+            })
+          `}
+        </Script>
+
       <Component {...pageProps}/>
     </Layout>
   )
