f8bcdd7846352b8a0a970ef1a9894afd439eb3bf
olingomaxwell1999 <olingomaxwell1999@gmail.com>
2023-11-01 11:33:18 +0300

    enqurre strip update

--- components/Enquirestrip.jsx
+++ components/Enquirestrip.jsx
@@ -1,16 +1,15 @@
-import React,{useEffect} from 'react'
+import React, { useEffect } from "react";
 // import AOS from 'aos'
 // import 'aos/dist/aos.css'
-import Link from 'next/link'
+import Link from "next/link";
 
 function Enquirestrip() {
-
   // useEffect(() => {
   //   AOS.init({duration:1500})
   // })
 
   return (
-    <div data-aos='zoom-in' className='enquirestrip'>
+    <div data-aos="zoom-in" className="enquirestrip">
       <div className="description-area">
         <div className="place">
           <h3>you are viewing</h3>
@@ -18,11 +17,11 @@ function Enquirestrip() {
         </div>
         <div className="place">
           <h3>prices from</h3>
-          <h2>KSHS. 2M</h2>
+          <h2>KSHS. 3.4M</h2>
         </div>
         <div className="place">
           <h3>completion date</h3>
-          <h2>Q1 2024</h2>
+          <h2>Q1 2026</h2>
         </div>
         <div className="place">
           <h3>payment over</h3>
@@ -33,16 +32,19 @@ function Enquirestrip() {
           <h2>Nairobi West</h2>
         </div>
       </div>
-      
+
       <div className="btn-area">
         <div className="place">
-          <Link href='https://share-eu1.hsforms.com/1PWrim3F1TeieOy8zdLFTFQfblz8' target='_blank'>
+          <Link
+            href="https://share-eu1.hsforms.com/1PWrim3F1TeieOy8zdLFTFQfblz8"
+            target="_blank"
+          >
             <button className="btn">register interest</button>
           </Link>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Enquirestrip
+export default Enquirestrip;
