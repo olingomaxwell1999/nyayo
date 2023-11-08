84e6d4150c2db3ecfe9035d7df8f2ff210a7654b
olingomaxwell1999 <olingomaxwell1999@gmail.com>
2023-11-08 19:12:05 +0300

    spaces

--- components/Onebedroomfirst.jsx
+++ components/Onebedroomfirst.jsx
@@ -18,9 +18,9 @@ function Onebedroomfirst() {
         {/* <p>The light filled, modern masterpiece 1 bedroom, is well designed with floor to ceiling windows offering amazing views to Nyayo Stadium and CBD. The bedroom boasts of a well built in robes and are serviced by spacious bathrooms. These units sits on a built up areas of approximately<strong> 280sqft</strong> for a <strong> junior 1BR, 300sqft </strong>for  <strong>1BR</strong> and <strong>340sqft </strong>for a <strong>Deluxe 1BR</strong></p> */}
 
         <p>
-          Our <strong>452SQFT</strong>for an
-          <strong>Executive One Bedroom</strong>and
-          <strong>398SQFT</strong>for an <strong>Executive One Bedroom</strong>
+          Our <strong> 452SQFT </strong> for an
+          <strong>Executive One Bedroom </strong> and
+          <strong> 398SQFT </strong> for a <strong> Premium One Bedroom</strong>
           apartments provide spacious, well-lit living areas, promising a
           contemporary and elegant residential experience. Investors can
           anticipate excellent returns from this highly coveted property.
